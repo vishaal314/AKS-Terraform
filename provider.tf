ce21e940597eb2e27018cbef4d488f6d6238acd9
vishaal314 <37283997+vishaal314@users.noreply.github.com>
2022-10-02 12:17:43 +0200

    Update provider.tf
    check the changes

--- provider.tf
+++ provider.tf
@@ -1,15 +1,12 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-  }
-  provider "azurerm" {
-  skip_provider_registration = true
-}
-
-}
-provider "azurerm" {
-  features {}
-}
+"terraform"{
+   "required_providers"{
+      "azurerm ="{
+         "source =""hashicorp/azurerm""version =""~>2.0"
+      }
+   }"provider""azurerm"{
+      "features"{
+         
+      }
+   }"provider""azurerm"{
+      "skip_provider_registration = true"
+   }
