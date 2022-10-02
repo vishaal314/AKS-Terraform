007d9652c2708227e81d0d4dabfdd61f1c24662c
vishaal314 <37283997+vishaal314@users.noreply.github.com>
2022-10-02 12:31:13 +0200

    Update provider.tf
    okay

--- provider.tf
+++ provider.tf
@@ -2,13 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~>2.0"
+      version = "~>=3.0"
     }
 }
 provider "azurerm" {
   features {}
-}
-provider "azurerm" {
-  skip_provider_registration = true
 }
 }
